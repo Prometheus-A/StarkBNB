1b06230b48a50736916ee24236616a2ea4d63a9f
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2025-01-01 22:57:27 +0100

    organized the host page a bit

--- StarkBnB/frontend/src/app/host/page.tsx
+++ StarkBnB/frontend/src/app/host/page.tsx
@@ -1,7 +1,10 @@
 const HostPage = () => {
     return ( 
-        <div>
-            Hello, Host
+        <div className="w-fit flex flex-col items-center justify-center gap-5 mx-auto mt-[10%]">
+            <p>Nothing else will be in this page for now, List a Property below to see stuff</p>
+            <button className="bg-blue-500 text-white rounded-md mx-auto w-fit px-4 py-2">
+                List A Property
+            </button>
         </div>
      );
 }
